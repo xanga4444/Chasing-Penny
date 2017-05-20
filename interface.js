4c904713faa6a1f00b3411258faa0665ad41a531
prtx <prtx.pratikshrestha@gmail.com>
2017-05-20 19:16:58 +0545

    Fill, clear cell

--- interface.js
+++ interface.js
@@ -4,3 +4,16 @@ var height = canvas_grid.height
 var cell  = 20
 var columns   = width/cell
 var rows   = height/cell
+
+
+function fillCell(xposition, yposition) {
+	ctx.fillStyle = '#abffab'
+	ctx.fillRect(xposition*cell, yposition*cell, cell, cell)
+}
+
+function clearCell(xposition, yposition) {
+	ctx.clearRect(xposition*cell, yposition*cell, cell, cell)
+}
+
+// fillCell(0, 2)
+// clearCell(0, 2)
