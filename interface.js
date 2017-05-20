70dd5d25c7aaeb29d85508e4efb349126ce1ca39
prtx <prtx.pratikshrestha@gmail.com>
2017-05-20 19:37:58 +0545

    Cell objects with directions

--- interface.js
+++ interface.js
@@ -1,19 +1,60 @@
-var ctx = canvas_grid.getContext('2d')
-var width  = canvas_grid.width
-var height = canvas_grid.height
-var cell  = 20
-var columns   = width/cell
-var rows   = height/cell
+var ctx = canvas_grid.getContext('2d');
+var width  = canvas_grid.width;
+var height = canvas_grid.height;
+var cell  = 20;
+var columns   = width/cell;
+var rows   = height/cell;
 
 
-function fillCell(xposition, yposition) {
-	ctx.fillStyle = '#abffab'
-	ctx.fillRect(xposition*cell, yposition*cell, cell, cell)
+function obj(xposition, yposition) {
+
+
+	this.xposition = xposition;
+	this.yposition = yposition;
+
+
+	this.fillCell = function() {
+		ctx.fillStyle = '#abffab'
+		ctx.fillRect(this.xposition*cell, this.yposition*cell, cell, cell)
+	}
+
+
+	this.clearCell = function() {
+		ctx.clearRect(this.xposition*cell, this.yposition*cell, cell, cell)
+	}
+
+
+	this.cellUp = function() {
+		this.clearCell();
+		this.yposition--;
+		this.fillCell();
+	}
+
+
+	this.cellDown = function() {
+		this.clearCell();
+		this.yposition++;
+		this.fillCell();
+	}
+
+
+	this.cellLeft = function() {
+		this.clearCell();
+		this.xposition--;
+		this.fillCell();
+	}
+
+
+	this.cellRight = function() {
+		this.clearCell();
+		this.xposition--;
+		this.fillCell();
+	}
+
 }
 
-function clearCell(xposition, yposition) {
-	ctx.clearRect(xposition*cell, yposition*cell, cell, cell)
-}
 
-// fillCell(0, 2)
-// clearCell(0, 2)
+a = new obj(0, 1);
+
+a.fillCell()
+a.cellDown()
