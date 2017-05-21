8922725c7dd6d21673cbb4785363b1fb34aa389b
prtx <prtx.pratikshrestha@gmail.com>
2017-05-21 16:08:19 +0545

    Moving objects and walls

--- interface.js
+++ interface.js
@@ -1,60 +1,102 @@
 var ctx = canvas_grid.getContext('2d');
 var width  = canvas_grid.width;
 var height = canvas_grid.height;
-var cell  = 20;
+var cell  = 12;
+var step = 3;
 var columns   = width/cell;
 var rows   = height/cell;
 
 
-function obj(xposition, yposition) {
+function obj(xposition, yposition, color) {
 
 
 	this.xposition = xposition;
 	this.yposition = yposition;
-
+	this.color = color;
+	
 
 	this.fillCell = function() {
-		ctx.fillStyle = '#abffab'
-		ctx.fillRect(this.xposition*cell, this.yposition*cell, cell, cell)
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.xposition, this.yposition, cell, cell)
 	}
 
 
 	this.clearCell = function() {
-		ctx.clearRect(this.xposition*cell, this.yposition*cell, cell, cell)
+		ctx.clearRect(this.xposition, this.yposition, cell, cell)
 	}
 
-
-	this.cellUp = function() {
-		this.clearCell();
-		this.yposition--;
-		this.fillCell();
-	}
-
-
-	this.cellDown = function() {
-		this.clearCell();
-		this.yposition++;
-		this.fillCell();
-	}
-
-
-	this.cellLeft = function() {
-		this.clearCell();
-		this.xposition--;
-		this.fillCell();
-	}
-
-
-	this.cellRight = function() {
-		this.clearCell();
-		this.xposition--;
-		this.fillCell();
-	}
+	this.fillCell();
 
 }
 
 
-a = new obj(0, 1);
+function movingObj(xposition, yposition, color) {
 
-a.fillCell()
-a.cellDown()
+
+	object = new obj(xposition, yposition, color);
+
+
+	object.cellUp = function() {
+		this.clearCell();
+		this.yposition -= step;
+		this.fillCell();
+	}
+
+
+	object.cellDown = function() {
+		this.clearCell();
+		this.yposition += step;
+		this.fillCell();
+	}
+
+
+	object.cellLeft = function() {
+		this.clearCell();
+		this.xposition -= step;
+		this.fillCell();
+	}
+
+
+	object.cellRight = function() {
+		this.clearCell();
+		this.xposition += step;
+		this.fillCell();
+	}
+
+	return object
+
+}
+
+
+function collide(obj1, obj2) {
+	
+	if (obj1.xposition+cell>obj2.xposition && obj1.xposition<obj2.xposition+cell) {
+		if (obj1.yposition+cell>obj2.yposition && obj1.yposition<obj2.yposition+cell) {	
+			console.log(true)
+		}
+	}
+}
+
+
+function keyBinding(event) {
+	switch (event.keyCode) {
+        case 37: a.cellLeft(); break // LEFT
+        case 38: a.cellUp(); break // UP
+        case 39: a.cellRight(); break // RIGHT
+        case 40: a.cellDown(); break // DOWN
+    }
+    collide(a, b)
+}
+
+
+a = new movingObj(0, 0, '#abffab');
+
+window.addEventListener("keydown", keyBinding, true)
+
+walls = []
+for (i = 0; i < 64; i++) {
+	wallColumn = Math.floor((Math.random() * columns) + 1)
+	wallRow = Math.floor((Math.random() * rows) + 1)
+	walls.push(new obj(wallColumn*cell, wallRow*cell, '#333333'));
+} 
+
