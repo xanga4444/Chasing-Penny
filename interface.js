dcae5b03e035bbcc1960fbf1a3a446d0a4ca5042
prtx <prtx.pratikshrestha@gmail.com>
2017-05-23 12:41:38 +0545

    Add basic computer movements

--- interface.js
+++ interface.js
@@ -5,21 +5,23 @@ var cell  = 12;
 var step = 4;
 var columns   = width/cell;
 var rows   = height/cell;
+var LEFT = [-1, 0];
+var UP = [0, -1];
+var RIGHT = [1, 0];
+var DOWN = [0, 1];
+var DIRECTIONS = [LEFT, UP, RIGHT, DOWN]
 
 
 function obj(xposition, yposition, color) {
 
-
 	this.xposition = xposition;
 	this.yposition = yposition;
-	this.color = color;
-	
+	this.color = color;	
 
 	this.fillCell = function() {
 		ctx.fillStyle = this.color;
 		ctx.fillRect(this.xposition, this.yposition, cell, cell)
 	}
-
 
 	this.clearCell = function() {
 		ctx.clearRect(this.xposition, this.yposition, cell, cell)
@@ -30,11 +32,9 @@ function obj(xposition, yposition, color) {
 }
 
 
-function movingObj(xposition, yposition, color, [LEFT, UP, RIGHT, DOWN]) {
-
+function movingObj(xposition, yposition, color) {
 
 	object = new obj(xposition, yposition, color);
-
 
 	object.move = function(DIRECTION) {
 
@@ -44,36 +44,31 @@ function movingObj(xposition, yposition, color, [LEFT, UP, RIGHT, DOWN]) {
 		
 		object.clearCell();
 
-		switch (DIRECTION) {
-			case LEFT: object.xposition -= step; break // LEFT
-			case UP: object.yposition -= step; break // UP
-			case RIGHT: object.xposition += step; break // RIGHT
-			case DOWN: object.yposition += step; break // DOWN
-		}
+		object.xposition += step*DIRECTION[0];
+		object.yposition += step*DIRECTION[1];
 
 		object.fillCell();
-	}
 
+	}
 
 	function validMovement(DIRECTION) {
 
+		switch (DIRECTION) {
+			case LEFT: if (object.xposition===0) {return false} break;
+			case UP: if (object.yposition===0) {return false} break;
+			case RIGHT: if (object.xposition+cell===width) {return false} break;
+			case DOWN: if (object.yposition+cell===height) {return false} break;
+		}
+
 		for (i = 0; i < 64; i++) {
 			if (collide(walls[i], DIRECTION)) {
-				return false
+				return false;
 			}
 		}
-		return true
-	}
 
-
-	object.keyBinding = function(event) {
-		
-		DIRECTION = event.keyCode;
-		object.move(DIRECTION);
+		return true;
 
 	}
-	window.addEventListener("keydown", object.keyBinding, true)
-
 
 	function collide(obj, DIRECTION) {
 
@@ -82,12 +77,8 @@ function movingObj(xposition, yposition, color, [LEFT, UP, RIGHT, DOWN]) {
 		y1 = object.yposition;
 		y2 = obj.yposition;
 
-		switch (DIRECTION) {
-			case LEFT: x1 -= step; break // LEFT
-			case UP: y1 -= step; break // UP
-			case RIGHT: x1 += step; break // RIGHT
-			case DOWN: y1 += step; break // DOWN
-		}
+		x1 += step*DIRECTION[0];
+		y1 += step*DIRECTION[1];
 
 		if (x1+cell>x2 && x1<x2+cell) {
 			if (y1+cell>y2 && y1<y2+cell) {	
@@ -103,6 +94,47 @@ function movingObj(xposition, yposition, color, [LEFT, UP, RIGHT, DOWN]) {
 }
 
 
+function computerPlayer(xposition, yposition, color) {
+
+	computerMovingObject = new movingObj(xposition, yposition, color);
+
+	var stepsTaken = 10;
+	setInterval(function(){
+		if (stepsTaken===10) {
+			DIRECTION = DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)]
+			stepsTaken = 0;
+		}
+		computerMovingObject.move(DIRECTION)
+		stepsTaken++;
+	}, 10);
+
+	return computerMovingObject
+
+}
+
+
+function userPlayer(xposition, yposition, color) {
+
+	userMovingObject = new movingObj(xposition, yposition, color);
+
+	userMovingObject.keyBinding = function(event) {
+
+		switch (event.keyCode) {
+	        case 37: DIRECTION = LEFT; break // LEFT
+		    case 38: DIRECTION = UP; break // UP
+		    case 39: DIRECTION = RIGHT; break // RIGHT
+		    case 40: DIRECTION = DOWN; break // DOWN
+		}
+		
+		userMovingObject.move(DIRECTION);
+	}
+	window.addEventListener("keydown", userMovingObject.keyBinding, true)
+
+	return userMovingObject
+
+}
+
+
 walls = []
 for (i = 0; i < 64; i++) {
 	wallColumn = Math.floor((Math.random() * columns) + 1)
@@ -110,12 +142,4 @@ for (i = 0; i < 64; i++) {
 	walls.push(new obj(wallColumn*cell, wallRow*cell, '#333333'));
 } 
 
-b = new movingObj(100, 100, '#abffab', [65, 87, 68, 83]);
-a = new movingObj(0, 0, '#abffab', [37, 38, 39, 40]);
-a.move(39)
-a.move(39)
-a.move(39)
-a.move(39)
-a.move(39)
-a.move(39)
-a.move(39)
+penny = new computerPlayer(100, 100, '#abffab');
